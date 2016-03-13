3ae2770d3d9151eddada4d44a66f7f2a868c68c1
Vitor <vitorandreazza@hotmail.com>
2016-03-13 02:55:55 -0300

    Inicio

---
+++ public_html/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('buscaIp',[]);
+
+app.controller('buscaIpController', function($scope,$http){
+    $scope.buscaLocalizacao = function(ip){
+                $scope.todosDados = (localStorage.getItem('todosDados')!==null)?
+                JSON.parse(localStorage.getItem('todosDados')):{};
+        $http({method: 'GET', url:'http://api.ipinfodb.com/v3/ip-city/?key=3d8ab80ef644b6b0687849e3e078a61c466260d4828e783c0301391a8d2ff347&ip='+ip+'&format=json'})
+                .success(function(data){
+                    $scope.dados = data;
+                    $scope.todosDados.push(dados);
+                    localStorage.setItem('todosDados', angular.toJson($scope.todosDados));
+                    $scope.dados = {};
+                })
+                .error(function(){
+                    alert("Não foi possível obter a localização!");
+                });
+    };
+    $scope.meuIp = function(ip){
+        $http({method: 'GET', url:'https://api.ipify.org?format=json'})
+                .success(function(data){
+                $scope.buscaLocalizacao(data.ip);  
+        })
+                .error(function(){
+                    alert("Não foi possível obter seu IP!");
+                });
+    };
+});
